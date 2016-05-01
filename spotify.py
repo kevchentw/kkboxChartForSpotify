96c9da7bb8c23040d4afd32d8782ed118f90c4e9
Kevin Chen <me@kevchentw.com>
2016-05-02 02:35:55 +0800

    add try except

--- spotify.py
+++ spotify.py
@@ -1,7 +1,7 @@
 import spotipy
 from settings import settings
 import spotipy.util as util
-
+from time import sleep
 
 class Spotify:
     def __init__(self):
@@ -19,23 +19,46 @@ class Spotify:
     def get_token(self, scope):
         token = util.prompt_for_user_token(self.username, scope)
         if token:
-            s = self.tokens['scope'] = spotipy.Spotify(auth=token)
-            return s
+            try:
+                s = self.tokens['scope'] = spotipy.Spotify(auth=token)
+                return s
+            except:
+                print("spotify api error, sleep 5s...")
+                sleep(5)
+                return get_token(scope)
         else:
             return None
 
     def add_tracks(self, tracks, playlist):
         scope = 'playlist-modify-public playlist-modify-private'
         s = self.get_spotify(scope)
-        r = s.user_playlist_add_tracks(self.username, playlist, tracks)
+        try:
+            r = s.user_playlist_add_tracks(self.username, playlist, tracks)
+        except:
+            print("spotify api error, sleep 5s...")
+            sleep(5)
+            r = self.add_tracks(tracks, playlist)
 
     def removes_tracks(self, tracks, playlist):
         scope = 'playlist-modify-public playlist-modify-private'
         s = self.get_spotify(scope)
-        r = s.user_playlist_remove_all_occurrences_of_tracks(self.username, playlist, tracks)
+        try:
+            r = s.user_playlist_remove_all_occurrences_of_tracks(self.username, playlist, tracks)
+        except:
+            print("spotify api error, sleep 5s...")
+            sleep(5)
+            r = self.removes_tracks(tracks, playlist)
+
 
     def search_track(self, title, artist):
-        results = self.spotify.search(q='artist:' + artist + " track:" + title, type='track')
+        q = 'artist:' + artist + " track:" + title
+        print(q)
+        try:
+            results = self.spotify.search(q=q, type='track')
+        except:
+            print("spotify api error, sleep 5s...")
+            sleep(5)
+            self.search_track(title, artist)
         tracks = results['tracks']['items']
         if len(tracks) > 0:
             return tracks[0]
@@ -53,7 +76,12 @@ class Spotify:
     def get_playlist_tracks(self, playlist):
         scope = 'playlist-modify-public playlist-modify-private'
         s = self.get_spotify(scope)
-        p = s.user_playlist_tracks(self.username, playlist, "items.track.id")
+        try:
+            p = s.user_playlist_tracks(self.username, playlist, "items.track.id")
+        except:
+            print("spotify api error, sleep 5s...")
+            sleep(5)
+            r = self.get_playlist_tracks(playlist)
         tracks = []
         for i in p['items']:
             tracks.append(i['track']['id'])
